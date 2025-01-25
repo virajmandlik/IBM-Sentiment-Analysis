1cb184ca86ef524df177841032a4aa018aa49724
=viraj <=viraj.mandlik@mitaoe.ac.in>
2025-01-26 00:56:27 +0530

    some changes

--- Client/src/components/Users.tsx
+++ Client/src/components/Users.tsx
@@ -1,13 +1,6 @@
 import * as React from "react";
 import { Button } from "@/components/ui/button";
-import RecordRTC from "recordrtc";
-import {
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
@@ -28,8 +21,7 @@ const users: SponsorProps[] = [
   {
     name: "Wellness Enthusiasts",
     description: "An individual pursuing mental wellness evaluation.",
-    avatarUrl:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQe2ViBkmacEAhE66woBFCerF6qqgtQGEB-AA&s",
+    avatarUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQe2ViBkmacEAhE66woBFCerF6qqgtQGEB-AA&s",
     isMentalDoctor: false,
   },
   {
@@ -56,94 +48,6 @@ const AnalyzeComponent = ({
   onClose: () => void;
   onResults: (results: any) => void;
 }) => {
-  const [isRecording, setIsRecording] = React.useState<boolean>(false);
-  const [transcription, setTranscription] = React.useState<string | null>(null);
-  const [error, setError] = React.useState<string | null>(null);
-  const recorderRef = React.useRef<RecordRTC | null>(null);
-
-  const handleToggleRecording = (
-    event: React.MouseEvent<HTMLButtonElement>,
-    field: string
-  ) => {
-    event.preventDefault(); // Prevent the page from refreshing
-
-    console.log("Starting recording...");
-
-    if (!isRecording) {
-      // Ask for microphone permission
-      navigator.mediaDevices
-        .getUserMedia({ audio: true })
-        .then((stream) => {
-          console.log("Media stream received");
-          recorderRef.current = new RecordRTC(stream, {
-            type: "audio",
-            mimeType: "audio/webm",
-          });
-          recorderRef.current.startRecording();
-          setIsRecording(true);
-          setError(null);
-        })
-        .catch((err) => {
-          // Handle the case where the user denies the permission
-          if (err.name === "NotAllowedError") {
-            console.error("Permission denied: Microphone access is required.");
-            setError("Permission denied: Microphone access is required.");
-          } else {
-            console.error("Error accessing microphone:", err);
-            setError("Error accessing microphone");
-          }
-        });
-    } else {
-      // Stop recording and upload
-      console.log("Stopping recording...");
-      recorderRef.current?.stopRecording(() => {
-        const blob = recorderRef.current?.getBlob();
-        console.log("Blob created:", blob);
-        if (blob) {
-          const file = new File([blob], "audio.webm", { type: "audio/webm" });
-
-          const formData = new FormData();
-          formData.append("audio", file);
-
-          fetch("http://localhost:3000/transcribe", {
-            method: "POST",
-            body: formData,
-          })
-            .then((response) => {
-              if (!response.ok) {
-                throw new Error("Failed to transcribe audio");
-              }
-              return response.json();
-            })
-            .then((data) => {
-              const transcript =
-                data.results[0]?.alternatives[0]?.transcript ||
-                "No transcription available";
-              setTranscription(transcript); // Store the transcription in state
-              console.log(transcript);
-              // Update the respective input field based on the 'field' parameter
-              if (field === "feeling") {
-                setFeeling(transcript);
-              } else if (field === "challenge") {
-                setChallenge(transcript);
-              } else if (field === "improve") {
-                setImprove(transcript);
-              }
-
-              setError(null);
-            })
-            .catch((err) => {
-              console.error("Error uploading file:", err);
-              setError("Failed to transcribe audio");
-            });
-        } else {
-          console.error("No blob available for the recording");
-        }
-      });
-      setIsRecording(false);
-    }
-  };
-
   const [feeling, setFeeling] = React.useState("");
   const [challenge, setChallenge] = React.useState("");
   const [improve, setImprove] = React.useState("");
@@ -238,89 +142,40 @@ const AnalyzeComponent = ({
         <CardContent>
           {user.isMentalDoctor ? (
             <div>
-              <Label
-                htmlFor="csv-upload"
-                className="text-gray-900 dark:text-gray-100"
-              >
+              <Label htmlFor="csv-upload" className="text-gray-900 dark:text-gray-100">
                 Upload CSV
               </Label>
-              <Input
-                type="file"
-                id="csv-upload"
-                onChange={handleCSVUpload}
-                accept=".csv"
-              />
+              <Input type="file" id="csv-upload" onChange={handleCSVUpload} accept=".csv" />
             </div>
           ) : (
             <form className="space-y-4">
-              <Label
-                htmlFor="feeling"
-                className="text-gray-900 dark:text-gray-100"
-              >
+              <Label htmlFor="feeling" className="text-gray-900 dark:text-gray-100">
                 How are you feeling today?
               </Label>
-
-              <div className="flex gap-2">
-                <Input
-                  id="feeling"
-                  value={feeling}
-                  onChange={(e) => setFeeling(e.target.value)}
-                  placeholder="I'm feeling..."
-                />
-                <Button
-                  variant="outline"
-                  onClick={(e) => handleToggleRecording(e, "feeling")}
-                  className="p-2"
-                >
-                  🎤
-                </Button>
-              </div>
-
-              <Label
-                htmlFor="challenge"
-                className="text-gray-900 dark:text-gray-100"
-              >
+              <Input
+                id="feeling"
+                value={feeling}
+                onChange={(e) => setFeeling(e.target.value)}
+                placeholder="I'm feeling..."
+              />
+              <Label htmlFor="challenge" className="text-gray-900 dark:text-gray-100">
                 Biggest concern on your mind?
               </Label>
-
-              <div className="flex gap-2">
-                <Input
-                  id="challenge"
-                  value={challenge}
-                  onChange={(e) => setChallenge(e.target.value)}
-                  placeholder="My biggest challenge is..."
-                />
-                <Button
-                  variant="outline"
-                  onClick={(e) => handleToggleRecording(e, "challenge")}
-                  className="p-2"
-                >
-                  🎤
-                </Button>
-              </div>
-
-              <Label
-                htmlFor="improve"
-                className="text-gray-900 dark:text-gray-100"
-              >
+              <Input
+                id="challenge"
+                value={challenge}
+                onChange={(e) => setChallenge(e.target.value)}
+                placeholder="My biggest challenge is..."
+              />
+              <Label htmlFor="improve" className="text-gray-900 dark:text-gray-100">
                 What would improve your current situation?
               </Label>
-
-              <div className="flex gap-2">
-                <Input
-                  id="improve"
-                  value={improve}
-                  onChange={(e) => setImprove(e.target.value)}
-                  placeholder="I would feel better if..."
-                />
-                <Button
-                  variant="outline"
-                  onClick={(e) => handleToggleRecording(e, "improve")}
-                  className="p-2"
-                >
-                  🎤
-                </Button>
-              </div>
+              <Input
+                id="improve"
+                value={improve}
+                onChange={(e) => setImprove(e.target.value)}
+                placeholder="I would feel better if..."
+              />
             </form>
           )}
         </CardContent>
@@ -328,10 +183,7 @@ const AnalyzeComponent = ({
           <Button variant="outline" onClick={onClose}>
             Cancel
           </Button>
-          <Button
-            onClick={handleSubmit}
-            disabled={user.isMentalDoctor && !csvFile}
-          >
+          <Button onClick={handleSubmit} disabled={user.isMentalDoctor && !csvFile}>
             {loading ? "Processing..." : "Submit"}
           </Button>
         </CardFooter>
@@ -342,9 +194,7 @@ const AnalyzeComponent = ({
 
 export const Users = () => {
   const { theme } = useTheme();
-  const [selectedUser, setSelectedUser] = React.useState<SponsorProps | null>(
-    null
-  );
+  const [selectedUser, setSelectedUser] = React.useState<SponsorProps | null>(null);
   const [analysisResults, setAnalysisResults] = React.useState<any>(null);
 
   const handleAnalyze = (user: SponsorProps) => {
@@ -357,9 +207,7 @@ export const Users = () => {
 
   return (
     <section id="users" className="container pt-24 sm:py-32">
-      <h2 className="text-center text-md lg:text-xl font-bold mb-8 text-primary">
-        Users
-      </h2>
+      <h2 className="text-center text-md lg:text-xl font-bold mb-8 text-primary">Users</h2>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-10 place-items-center">
         {users.map((user) => (
@@ -378,9 +226,7 @@ export const Users = () => {
             </CardHeader>
 
             {/* Description below the Avatar */}
-            <CardDescription className="text-center">
-              {user.description}
-            </CardDescription>
+            <CardDescription className="text-center">{user.description}</CardDescription>
 
             <CardFooter className="flex justify-center mt-4">
               <Button variant="outline" onClick={() => handleAnalyze(user)}>
--- Server/src/server.js
+++ Server/src/server.js
@@ -2,9 +2,9 @@ const express = require("express");
 const axios = require("axios");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const { loginToInstagram ,postToInstagram} = require("./instagram/index");
+const CronJob = require("cron").CronJob;
 const dotenv = require("dotenv");
-const multer = require("multer");
-const fs = require("fs");
 dotenv.config();
 
 const app = express();
@@ -12,26 +12,99 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-// Route for Wellness Enthusiasts
-app.post("/api/predict", async (req, res) => {
-  const { feeling, challenge, improve } = req.body;
-  console.log("The data received at backend is:", feeling, challenge, improve);
 
-  try {
-    const response = await axios.post("http://localhost:5000/predict", {
-      feeling,
-      challenge,
-      improve,
-    });
-    console.log("The data sending from backend is:", response.data);
 
-    // Forward the entire response from Flask to the frontend
-    res.json(response.data);
-  } catch (error) {
-    console.error("Error calling Flask API:", error.message);
-    res.status(500).json({ error: "Internal Server Error" });
+
+
+// Instagram login endpoint
+app.post("/api/instagram/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json({ error: "Username and password are required." });
+  }
+
+  const result = await loginToInstagram(username, password);
+
+  if (result.success) {
+    res.json({ message: `Logged in as ${result.username}` });
+  } else {
+    res.status(401).json({ error: result.error });
   }
 });
+
+const cronInsta = new CronJob("0 0 * * *", async () => {
+  console.log("Starting scheduled Instagram post...");
+  await postToInstagram("username", "password"); // Replace with real credentials
+});
+
+cronInsta.start();
+
+app.post("/api/instagram/post", async (req, res) => {
+  const { username, password, imageUrl, caption } = req.body;
+console.log('the data recievd at bakend for posting is ',username, password, imageUrl, caption)
+  if (!username || !password || !imageUrl || !caption) {
+    return res.status(400).json({ error: "All fields are required." });
+  }
+
+  try {
+    const result = await postToInstagram(username, password, imageUrl, caption);
+    if (result.success) {
+      res.json({ message: "Post uploaded successfully!" });
+    } else {
+      res.status(500).json({ error: "Failed to upload post." });
+    }
+  } catch (error) {
+    console.log("the error is ",error)
+    res.status(500).json({ error: "An error occurred while uploading the post." });
+  }
+});
+
+
+
+
+
+
+// Route for Wellness Enthusiasts
+// Route for Sentiment Analysis
+app.post("/api/predict", async (req, res) => {
+  const { feeling, challenge, improve, checkCaption } = req.body;
+  console.log("The data received at backend is:", feeling, challenge, improve, checkCaption);
+
+  // If checkCaption is provided, send it for sentiment analysis and ignore other fields
+  if (checkCaption) {
+    try {
+      console.log('The check caption is ', checkCaption);
+      const response = await axios.post("http://localhost:5000/predict", {
+        checkCaption: checkCaption,  // Send only checkCaption
+      });
+
+      console.log("The data sending from backend is:", response.data);
+      res.json(response.data);  // Forward the response from Flask API
+    } catch (error) {
+      console.error("Error calling Flask API:", error.message);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  } else {
+    // Otherwise, process the feeling, challenge, and improve fields
+    try {
+      const response = await axios.post("http://localhost:5000/predict", {
+        feeling,
+        challenge,
+        improve,
+      });
+      console.log("The data sending from backend is:", response.data);
+      res.json(response.data);  // Forward the response from Flask API
+    } catch (error) {
+      console.error("Error calling Flask API:", error.message);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  }
+});
+
+
+
+
 
 // Route for Counselors to process CSV data
 app.post("/api/predictPatientsSentiments", async (req, res) => {
@@ -54,12 +127,10 @@ app.post("/api/predictPatientsSentiments", async (req, res) => {
 
     try {
       // Send only the sentiment to the model
-      const response = await axios.post("http://localhost:5000/predict", {
-        feeling: sentiment,
-      });
+      const response = await axios.post("http://localhost:5000/predict", { feeling: sentiment });
 
       const result = {
-        name: name, // Name of the user
+        name: name,                       // Name of the user
         analyzedSentiment: response.data.overallSentiment, // Analyzed result from the model
       };
 
@@ -88,56 +159,6 @@ app.post("/api/predictPatientsSentiments", async (req, res) => {
   res.json(results);
 });
 
-// Set up multer for file uploads
-const upload = multer({ dest: "uploads/" });
-
-// Enable CORS
-app.use(cors({ origin: "http://localhost:5173" })); // Replace with your frontend origin
-
-// Environment variables for IBM Watson API
-const API_KEY = process.env.IBM_WATSON_API_KEY;
-const INSTANCE_URL = process.env.IBM_WATSON_URL;
-
-// API endpoint to handle audio file upload and transcription
-app.post("/transcribe", upload.single("audio"), async (req, res) => {
-  const audioFile = req.file;
-
-  if (!audioFile) {
-    return res.status(400).json({ error: "No audio file uploaded" });
-  }
-
-  try {
-    const audioData = fs.readFileSync(audioFile.path);
-
-    const contentType = audioFile.mimetype; // Dynamically set content type based on file
-
-    const response = await axios.post(
-      `${INSTANCE_URL}/v1/recognize`,
-      audioData,
-      {
-        headers: {
-          "Content-Type": contentType, // This should match the MIME type of the uploaded file
-        },
-        auth: {
-          username: "apikey",
-          password: API_KEY,
-        },
-      }
-    );
-
-    res.json(response.data);
-  } catch (error) {
-    console.error(
-      "Error transcribing audio:",
-      error.response?.data || error.message
-    );
-    res.status(500).json({ error: "Failed to transcribe audio" });
-  } finally {
-    fs.unlink(audioFile.path, (err) => {
-      if (err) console.error("Error cleaning up file:", err);
-    });
-  }
-});
 app.listen(3000, () => {
   console.log("Node.js server is running on port 3000");
 });
