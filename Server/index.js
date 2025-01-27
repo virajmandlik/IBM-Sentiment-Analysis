595555067b9cf16d4082a7137ef235d8637ed676
=viraj <=viraj.mandlik@mitaoe.ac.in>
2025-01-27 16:36:37 +0530

    some adjustment , and rebuild the dist folder

--- Server/index.js
+++ Server/index.js
@@ -18,7 +18,7 @@ app.use(cors({
   origin: "*", // Use '*' to allow all, but be cautious with sensitive data
   credentials: false, // Set to true if you need cookies or Authorization headers
 }));
-
+app.use(cors({ origin: "http://localhost:5173" }));
 // Environment variables for IBM Watson API
 const API_KEY = process.env.IBM_WATSON_API_KEY;
 const INSTANCE_URL = process.env.IBM_WATSON_URL;
